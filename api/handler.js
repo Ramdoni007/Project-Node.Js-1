a1dfd36a5654560c20878418b8a9559478179fdc
Ramdoni007 <ramd42156@gmail.com>
2021-03-09 06:44:02 +0700

    Refactoring Kode dengan membuat Handle dan Routes Secara Terpisah :)

---
+++ api/handler.js
@@ -0,0 +1,57 @@
+const { ObjectID } = require('bson');
+const {ObjectId} = require('mongodb');
+
+exports.addNote = ((req,res) => { 
+    const{notesCollection} = req.app.locals;
+    // Code Untuk Mencaru Data Ke Collections
+
+    notesCollection.insertOne(req.body).then((result) => { 
+        console.log(result);
+    }); 
+    res.status(200).json('Data Berhasil di Simpan Ya:)');
+});
+
+
+exports.getAllNotes = ((req,res) => { 
+    const {notesCollection} = req.app.locals;
+
+    // kode mencari semua notes 
+    notesCollection
+    .find()
+    .toArray()
+    .then((result) => { 
+        res.status(200).json(result);
+    });
+});
+
+
+exports.getNote = ((req,res) => { 
+    const {notesCollection} = req.app.locals;
+
+    // kode Mencari Notes Seusai dengan Id : 
+    notesCollection.findOne({_id:ObjectId(req.params.id)}).then((result)=> { 
+        res.status(200).json(result);
+    }) ; 
+});
+
+exports.updateNote = ((req,res) => { 
+    const {notesCollection} = req.app.locals; 
+
+    // Kode Untuk Update Data Collections 
+    notesCollection 
+    .updateNote({_id:ObjectID(req.params.id)}, {$set: {title:req.body.title, note:req.body.note}});  
+    .then ((result) => { 
+        console.log(result);
+    });
+}) ;
+
+
+exports.deleteNote = ((req,res) => { 
+    const {notesCollection} = req.app.locals; 
+
+    // Kode Untuk Mendelete Data Collections 
+    notesCollection.deleteOne({_id:ObjectID(req.params.id)}).then((result)=> { 
+        console.log(result);
+    });
+    res.status(200).json(' Yeyyyy Data Berhasil di Hapus')
+});
--- api/route.js
+++ api/route.js
@@ -1,68 +1,16 @@
 const express = require('express');
-const {ObjectId} = require ('mongodb'); 
+
 
 const router = express.Router(); 
 
-
-router.post('/note',(req,res) => { 
-    const { notesCollection } = req.app.locals;
-
-    // Cara Insert Data Ke Collection 
-    notesCollection.insertOne(req.body).then((result) => { 
-        console.log(result);
-    });  
-
-    res.status(200).json('Data Berhasil di Simpan');
-});
-
-router.get('/notes',(req,res) => { 
-    const { notesCollection} = req.app.locals; 
-
-    // find All Notes 
-    notesCollection 
-    .find()
-    .toArray()
-    .then((result) => {
-        res.status(200).json(result);
-    });
-});
+const {addNote,getAllNote,getNote,updateNote,deleteNote} = require('./handler');
 
 
-router.get('/notes/:id',(req,res) => {
-    const {notesCollection} = req.app.locals; 
-
-
-    // Mencari Notes Berdasarkan Id nya 
-    notesCollection.findOne({_id:ObjectId(req.params.id)}).then((result) => { 
-        res.status(200).json(result);
-    });
-});
-
-
-
-router.put('/note/:id',(req,res) => { 
-    const {notesCollection} = req.app.locals;
-
-    // Cara Update data Collections 
-    notesCollection
-    .updateOne({_id:ObjectId(req.params.id)}, {$set: {title: req.body.title, note: req.body.note }})
-    .then((result) => { 
-    console.log(result);
-    }); 
-    res.status(200).json('dDA')
-    });
-
-router.delete('/note/:id',(req,res) => { 
-    const {notesCollection} = req.app.locals; 
-
-    // Cara Deleted data Collections 
-    notesCollection.deleteOne({_id:ObjectId(req.params.id)}).then((result) => { 
-        console.log(result);
-    })
-
-    res.status(200).json('Yey Data Berhasil di Hapus ')
-})
-
+router.post('/note',addNote);
+router.get('/notes',getAllNote);
+router.get('/note/:id',getNote);
+router.put('/note/:id',updateNote);
+router.delete('/note/:id',deleteNote);
 
 
 module.exports = router;
